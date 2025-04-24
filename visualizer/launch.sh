38e1beac16a3567be39ab8512b5e3301dec1a901
Noe <noe.duruz@gmail.com>
2025-04-24 10:28:55 +0200

    launch file update

--- visualizer/launch.sh
+++ visualizer/launch.sh
@@ -1,9 +1,15 @@
 # setup data with python scripts
-cd visualizer
-uv run scripts/split_gff.py data/data/GCF_014441545.1/genomic.gff -t genes
-uv run scripts/create_chromosome_dbs.py data/data/GCF_014441545.1/chromosomes NC_051805.1.gff
-uv run scripts/sequence.py --fasta data/data/GCF_014441545.1/GCF_014441545.1_ROS_Cfam_1.0_genomic.fna 
---chromosome NC_051805.1 --output cgpt-visualizer-website/src/data/chromosome_data.json
+# cd visualizer
+uv run scripts/split_gff.py \
+    data/ncbi_dataset/ncbi_dataset/data/GCF_014441545.1/genomic.gff \
+    -t gene
+uv run scripts/create_chromosome_dbs.py \
+    data/ncbi_dataset/ncbi_dataset/data/GCF_014441545.1/chromosomes \
+    NC_051805.1.gff
+uv run scripts/sequence.py \
+    --fasta data/ncbi_dataset/ncbi_dataset/data/GCF_014441545.1/GCF_014441545.1_ROS_Cfam_1.0_genomic.fna \
+    --chromosome NC_051805.1 \
+    --output cgpt-visualizer-website/src/data/chromosome_data.json
 
 # start the frontend
 cd cgpt-visualizer-website
