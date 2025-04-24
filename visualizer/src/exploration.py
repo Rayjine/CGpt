032c0d4f6571c80fbc1540a61aba8e2c91083c66
Noe <noe.duruz@gmail.com>
2025-04-24 19:41:17 +0200

    removed useless files

--- visualizer/src/exploration.py
+++
@@ -1,9 +0,0 @@
-from Bio import SeqIO
-
-for seq_record in SeqIO.parse(
-    "data/ncbi_dataset/data/GCF_014441545.1/GCF_014441545.1_ROS_Cfam_1.0_genomic.fna",
-    "fasta",
-):
-    print(seq_record.id)
-    print(repr(seq_record.seq))
-    print(len(seq_record))
